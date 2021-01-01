f34d3b6ded3ba4849550852d76ed5ec1854264d2
Albert Tian <alberttian@alberts-air.lan>
2020-12-31 22:02:10 -0500

    Migrated navbar to functional components...

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'gatsby-link';
 import Headroom from 'react-headroom';
 import classNames from 'classnames';
@@ -42,65 +42,109 @@ const PAGES = [
 ];
 
 
-export default class Navbar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      navOpen: false
-    };
+// export class DEPNavbar extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       navOpen: false
+//     };
 
-    this._handleHamburger = ::this._handleHamburger;
+//     this._handleHamburger = this._handleHamburger;
+//   }
+
+//   _handleHamburger(e) {
+//     if (e) e.preventDefault();
+//     this.setState({
+//       navOpen: !this.state.navOpen
+//     });
+//   }
+
+//   render() {
+//     const { type } = this.props;
+//       return (
+//         <div
+//           className={classNames("nav", {
+//             "nav--open": this.state.navOpen
+//           })}
+//         >
+//           <div className="nav__wrapper">
+//             <Link className="nav__item nav__logo" to='/'>
+//               <img src="/img/logo__icon--blue.svg"/>
+//             </Link>
+//             <div
+//               className={classNames("nav__item", "nav__hamburger", {
+//                 "nav__hamburger--active": this.state.navOpen
+//               })}
+//               onClick={this._handleHamburger}
+//             >
+//               <div className="hamburger__bar bar--1"></div>
+//               <div className="hamburger__bar bar--2"></div>
+//             </div>
+//             <div className="nav__links">
+//               {PAGES.map((page) => {
+//                 return (
+//                   <Link
+//                     className="nav__item nav__link"
+//                     to={`/${page.slug}`}
+//                     onClick={() => this._handleHamburger()}
+//                     key={page.slug}
+//                   >
+//                     {page.name}
+//                   </Link>
+//                 );
+//               })}
+//               {/* DEPRECATED: SUBMIT A REQUEST BUTTON */}
+//               {/* <Link className="nav__item nav__cta" to='/requests/'>
+//                 Submit a Request
+//               </Link> */}
+//             </div>
+//           </div>
+//         </div>
+//       );
+//   }
+// }
+
+export default function Navbar(props) {
+  const [ navOpen, setNavOpen ] = useState(false)
+  const { types } = props
+  function _handleHamburger(e) {
+    if (e) e.preventDefault()
+    setNavOpen(!navOpen)
   }
-
-  _handleHamburger(e) {
-    if (e) e.preventDefault();
-    this.setState({
-      navOpen: !this.state.navOpen
-    });
-  }
-
-  render() {
-    const { type } = this.props;
-      return (
+  return (
+    <div
+      className={classNames("nav", {
+        "nav--open": navOpen
+      })}
+    >
+      <div className="nav__wrapper">
+        <Link className="nav__item nav__logo" to='/'>
+          <img src="/img/logo__icon--blue.svg"/>
+        </Link>
         <div
-          className={classNames("nav", {
-            "nav--open": this.state.navOpen
+          className={classNames("nav__item", "nav__hamburger", {
+            "nav__hamburger--active": navOpen
           })}
+          onClick={_handleHamburger}
         >
-          <div className="nav__wrapper">
-            <Link className="nav__item nav__logo" to='/'>
-              <img src="/img/logo__icon--blue.svg"/>
-            </Link>
-            <div
-              className={classNames("nav__item", "nav__hamburger", {
-                "nav__hamburger--active": this.state.navOpen
-              })}
-              onClick={this._handleHamburger}
-            >
-              <div className="hamburger__bar bar--1"></div>
-              <div className="hamburger__bar bar--2"></div>
-            </div>
-            <div className="nav__links">
-              {PAGES.map((page) => {
-                return (
-                  <Link
-                    className="nav__item nav__link"
-                    to={`/${page.slug}`}
-                    onClick={() => this._handleHamburger()}
-                    key={page.slug}
-                  >
-                    {page.name}
-                  </Link>
-                );
-              })}
-              {/* DEPRECATED: SUBMIT A REQUEST BUTTON */}
-              {/* <Link className="nav__item nav__cta" to='/requests/'>
-                Submit a Request
-              </Link> */}
-            </div>
-          </div>
+          <div className="hamburger__bar bar--1"></div>
+          <div className="hamburger__bar bar--2"></div>
         </div>
-      );
-  }
+        <div className="nav__links">
+          {PAGES.map((page) => {
+            return (
+              <Link
+                className="nav__item nav__link"
+                to={`/${page.slug}`}
+                onClick={() => _handleHamburger()}
+                key={page.slug}
+              >
+                {page.name}
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  )
 }
-
