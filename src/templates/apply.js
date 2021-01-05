d2a169d697cc0ee74b8f9c9bcf4f11dd52958a05
Albert Tian <alberttian@alberts-air.lan>
2021-01-05 00:39:44 -0500

    With apply functional component page...

--- src/templates/apply.js
+++ src/templates/apply.js
@@ -1,17 +1,42 @@
 import React from 'react';
- 
+import { makeStyles } from '@material-ui/core/styles'
 
-export default class ApplyPage extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+const useStyles = makeStyles({
+  // SECTION 1
+  sectionONE: {
+    flex: "3",
+    textAlign: "center",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "calc(100vh - 60px)",
+    width: "100%",
+    padding: "0 20%",
+  },
+})
 
-  componentDidMount() {}
-
-  render () {
-
-    return (
-      <p>APPLY PAGE IN PROGRESS</p>
-    );
-  }
+export default function ApplyPage(props) {
+  const classes = useStyles()
+  return(
+    <div className={classes.sectionONE}>
+      <p>APPLY PAGE W FUNCTIONAL</p>
+    </div>
+  )
 }
+
+
+// export default class ApplyPage extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+
+//   componentDidMount() {}
+
+//   render () {
+
+//     return (
+//       <p>APPLY PAGE IN PROGRESS</p>
+//     );
+//   }
+// }
--- src/templates/index.js
+++ src/templates/index.js
@@ -3,57 +3,57 @@
 // import Script from 'react-load-script';
 // import Hero from '../components/Hero';
 // import ApplyWidget from '../components/ApplyWidget';
-import { makeStyles, styled } from "@material-ui/core"
+// import { makeStyles, styled } from "@material-ui/core/styles"
 
-const useStyles = makeStyles({
-  // SECTION 1
-  sectionONE: {
-    flex: "3",
-    textAlign: "center",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    height: "calc(100vh - 60px)",
-    width: "100%",
-    padding: "0 20%",
-  },
-  buttonWrapper: {
-      display: "flex",
-      minWidth: "300px",
-      width: "70%",
-      justifyContent: "center",
-      paddingTop: "30px",
-  },
-  linkButton: {
-    margin: "10px",
-    padding: "15px 30px",
-    borderRadius: "3px",
-    textDecoration: "none",
-    transition: "0.2s ease all",
-    boxShadow: "0px 5px 10px rgba(0, 0, 0, 0.1)",
-    backgroundColor: props => props.bcBefore,
-    color: props => props.textColor,
+// const useStyles = makeStyles({
+//   // SECTION 1
+//   sectionONE: {
+//     flex: "3",
+//     textAlign: "center",
+//     display: "flex",
+//     flexDirection: "column",
+//     justifyContent: "center",
+//     alignItems: "center",
+//     height: "calc(100vh - 60px)",
+//     width: "100%",
+//     padding: "0 20%",
+//   },
+//   buttonWrapper: {
+//       display: "flex",
+//       minWidth: "300px",
+//       width: "70%",
+//       justifyContent: "center",
+//       paddingTop: "30px",
+//   },
+//   linkButton: {
+//     margin: "10px",
+//     padding: "15px 30px",
+//     borderRadius: "3px",
+//     textDecoration: "none",
+//     transition: "0.2s ease all",
+//     boxShadow: "0px 5px 10px rgba(0, 0, 0, 0.1)",
+//     backgroundColor: props => props.bcBefore,
+//     color: props => props.textColor,
   
-    '&:hover': {
-      textDecoration: "none",
-      backgroundColor: props => props.bcAfter,
-      color: props => props.textColor,
-    },
-  },
-  subText: {
-    width: "40vw",
-    color: "black",
-    fontSize: "22px",
-  },
+//     '&:hover': {
+//       textDecoration: "none",
+//       backgroundColor: props => props.bcAfter,
+//       color: props => props.textColor,
+//     },
+//   },
+//   subText: {
+//     width: "40vw",
+//     color: "black",
+//     fontSize: "22px",
+//   },
 
-  // SECITON 2
-  sectionTWO: {
+//   // SECITON 2
+//   sectionTWO: {
 
-  }
-})
+//   }
+// })
 
-// // BUTTON FOR THE FRONTPAGE
+// BUTTON FOR THE FRONTPAGE
 // function LinkButton(props) {
 //   const { color, ...other } = props
 //   const LinkButtonClasses = useStyles(props)
@@ -63,28 +63,28 @@ const useStyles = makeStyles({
 // export default function IndexPage(props) {
 //   const classes = useStyles()
 
-//   function _handleNetlifyLoad() {
-//     const { netlifyIdentity } = window;
-//     if (netlifyIdentity) {
-//       netlifyIdentity.on('init', (user) => {
-//         if (!user) {
-//           netlifyIdentity.on('login', () => {
-//             document.location.href = '/admin/';
-//           });
-//         }
-//       });
-//     }
-//     netlifyIdentity.init();
-//   }
+  // function _handleNetlifyLoad() {
+  //   const { netlifyIdentity } = window;
+  //   if (netlifyIdentity) {
+  //     netlifyIdentity.on('init', (user) => {
+  //       if (!user) {
+  //         netlifyIdentity.on('login', () => {
+  //           document.location.href = '/admin/';
+  //         });
+  //       }
+  //     });
+  //   }
+  //   netlifyIdentity.init();
+  // }
 
 //   return (
 //     <div>
-//       <Script
+//       {/* <Script
 //         url="https://identity.netlify.com/v1/netlify-identity-widget.js"
 //         onLoad={() => _handleNetlifyLoad()}
-//       />
-//       <div className={classes.section1}>
-//         <p className={classes.subText}>Innovative Design is UC Berkeley’s premier creative agency. 
+//       /> */}
+//       <div className={classes.sectionONE}>
+//         <p className={classes.subText}>REMOVED NETFLIY AND GRAPHQL Innovative Design is UC Berkeley’s premier creative agency. 
 //           We are designers, photographers, and web developers 
 //           together in a mission to Make Berkeley Beautiful.
 //         </p>
@@ -119,90 +119,121 @@ const useStyles = makeStyles({
 //   }
 // `;
 
+// ------------------------------------------------------------------------------------------------------------------------
+
+// import React from 'react';
+// import Link from 'gatsby-link';
+// import Script from 'react-load-script';
+// import Hero from '../components/Hero';
+// import ApplyWidget from '../components/ApplyWidget';
+
+// function TestComponent() {
+//   const classes = useStyles()
+//   return (
+//     <div className={classes.sectionONE}>i fucking hate this</div>
+//   )
+// }
+
+// export default class IndexPage extends React.Component {
+//   _handleNetlifyLoad() {
+//     const { netlifyIdentity } = window;
+//     if (netlifyIdentity) {
+//       netlifyIdentity.on('init', (user) => {
+//         if (!user) {
+//           netlifyIdentity.on('login', () => {
+//             document.location.href = '/admin/';
+//           });
+//         }
+//       });
+//     }
+//     netlifyIdentity.init();
+//   }
+  
+//   render() {
+
+//     const {
+//       row_slides: rowSlides
+//     } = this.props.data.markdownRemark.frontmatter;
+
+//     return (<div className="index">
+//       <Script
+//         url="https://identity.netlify.com/v1/netlify-identity-widget.js"
+//         onLoad={() => this._handleNetlifyLoad()}
+//       />
+//       <TestComponent />
+
+//       <div className="hero">
+//         <img className="blob__center blob--yellow" src="img/sp20/blob--yellow.svg" />
+//         <div className="images__container images__container--center">
+//         </div>
+//         <div className="hero__center">
+//           <h1>What we do</h1>
+//         </div>
+//       </div>
+      
+//     <ApplyWidget data={this.props.widgetMeta} />
+//       <div className="row__container">
+//         {rowSlides.map((row) => {
+//           return (<div className={`row row__${row.type}`}>
+//             <img src={row.img} />
+//             <p>{row.caption}</p>
+//             <p>
+//               <Link to={row.link}>
+//                 {row.link_text} &rarr;
+//               </Link>
+//             </p>
+//           </div>);
+//         })}
+//       </div>
+//     </div>);
+//   }
+// }
+
+// export const pageQuery = graphql`
+//   query IndexQuery {
+//     markdownRemark(frontmatter: { path: { eq: "/" } }) {
+//       html
+//       frontmatter {
+//         hero_heading
+//         hero_subheading
+//         row_slides {
+//           img
+//           caption
+//           link
+//           link_text
+//           type
+//         }
+//       }
+//     }
+//   }
+// `;
+
+// ------------------------------------------------------------------------------------------------------------------------
+
 
 import React from 'react';
-import Link from 'gatsby-link';
-import Script from 'react-load-script';
-import Hero from '../components/Hero';
-import ApplyWidget from '../components/ApplyWidget';
+import { makeStyles } from '@material-ui/core/styles'
 
-function TestComponent() {
+const useStyles = makeStyles({
+  // SECTION 1
+  sectionONE: {
+    flex: "3",
+    textAlign: "center",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "calc(100vh - 60px)",
+    width: "100%",
+    padding: "0 20%",
+  },
+})
+
+export default function IndexPage() {
   const classes = useStyles()
-  return (
-    <div className={classes.sectionONE}>i fucking hate this</div>
+  return(
+    <div className={classes.sectionONE}>
+      <p>STRAIGNT CAP</p>
+    </div>
   )
 }
-
-export default class IndexPage extends React.Component {
-  _handleNetlifyLoad() {
-    const { netlifyIdentity } = window;
-    if (netlifyIdentity) {
-      netlifyIdentity.on('init', (user) => {
-        if (!user) {
-          netlifyIdentity.on('login', () => {
-            document.location.href = '/admin/';
-          });
-        }
-      });
-    }
-    netlifyIdentity.init();
-  }
-  
-  render() {
-
-    const {
-      row_slides: rowSlides
-    } = this.props.data.markdownRemark.frontmatter;
-
-    return (<div className="index">
-      <Script
-        url="https://identity.netlify.com/v1/netlify-identity-widget.js"
-        onLoad={() => this._handleNetlifyLoad()}
-      />
-      <TestComponent />
-
-      <div className="hero">
-        <img className="blob__center blob--yellow" src="img/sp20/blob--yellow.svg" />
-        <div className="images__container images__container--center">
-        </div>
-        <div className="hero__center">
-          <h1>What we do</h1>
-        </div>
-      </div>
-      
-    <ApplyWidget data={this.props.widgetMeta} />
-      <div className="row__container">
-        {rowSlides.map((row) => {
-          return (<div className={`row row__${row.type}`}>
-            <img src={row.img} />
-            <p>{row.caption}</p>
-            <p>
-              <Link to={row.link}>
-                {row.link_text} &rarr;
-              </Link>
-            </p>
-          </div>);
-        })}
-      </div>
-    </div>);
-  }
-}
-
-export const pageQuery = graphql`
-  query IndexQuery {
-    markdownRemark(frontmatter: { path: { eq: "/" } }) {
-      html
-      frontmatter {
-        hero_heading
-        hero_subheading
-        row_slides {
-          img
-          caption
-          link
-          link_text
-          type
-        }
-      }
-    }
-  }
-`;
