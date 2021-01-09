13611b2e168a2bcd1a5975b30fcfb0ef3c9f68b2
Albert Tian <alberttian@Alberts-MacBook-Air.local>
2021-01-09 14:08:55 -0500

    Fixed footer with flexbox...

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import '../css/components/footer.scss'
 
 function Column(props) {
   return (
-    <div className="col">
+    <div className="column__wrapper">
       <h4 className="column__title">{props.title}</h4>
       <ul className="unstyled__list">
         {props.links.map((page) => (
@@ -38,46 +38,42 @@ function Column(props) {
 export default function Footer() {
   return (
     <div className="footer">
-      <div className="container">
-        <div className="row">
-          {/* LOGO*/}
-          <div className="col">
-            <Link to="/" className="banner__logo">
-              <img src="/img/Footer-Logo.svg" alt="Innovative Design"/>
-            </Link>
-          </div>
-          {/* STUDENTS */}
-          <Column
-            title="Students"
-            links={[
-              { name: "Apply", slug: "apply" },
-              { name: "About", slug: "about" },
-              { name: "Events", slug: "events" },
-              { name: "DeCals", slug: "decals" },
-              { name: "Chapters", slug: "chapters" },
-            ]}
-          />
-          {/* ORGS 4 */}
-          <Column
-            title="Organizations"
-            links={[
-              { name: "Apply", slug: "apply" },
-              { name: "Clients", slug: "clients" },
-            ]}
-          />
-          {/* LINKS 5 */}
-          <Column
-            title="Find Us"
-            links={[
-              { name: "Facebook", href: "https://www.facebook.com/InnovativeDesignUCB" },
-              { name: "Instagram", href: "https://www.instagram.com/innodatcal/" },
-              { name: "Behance", href: "https://www.behance.net/InnoD" },
-              { name: "Twitter", href: "https://twitter.com/InnoDatCAL" },
-            ]}
-          />
-        </div>
-
+      <div className="row__wrapper">
+        {/* LOGO*/}
+        <Link to="/" className="banner__logo">
+          <img src="/img/Footer-Logo.svg" alt="Innovative Design" style={{ width: "18em" }} />
+        </Link>
+        {/* STUDENTS */}
+        <Column
+          title="Students"
+          links={[
+            { name: "Apply", slug: "apply" },
+            { name: "About", slug: "about" },
+            { name: "Events", slug: "events" },
+            { name: "DeCals", slug: "decals" },
+            { name: "Chapters", slug: "chapters" },
+          ]}
+        />
+        {/* ORGS 4 */}
+        <Column
+          title="Organizations"
+          links={[
+            { name: "Apply", slug: "apply" },
+            { name: "Clients", slug: "clients" },
+          ]}
+        />
+        {/* LINKS 5 */}
+        <Column
+          title="Find Us"
+          links={[
+            { name: "Facebook", href: "https://www.facebook.com/InnovativeDesignUCB" },
+            { name: "Instagram", href: "https://www.instagram.com/innodatcal/" },
+            { name: "Behance", href: "https://www.behance.net/InnoD" },
+            { name: "Twitter", href: "https://twitter.com/InnoDatCAL" },
+          ]}
+        />
       </div>
+
     </div>
   )
 }
