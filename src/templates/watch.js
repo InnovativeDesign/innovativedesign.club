0cee209081864b397afd741c8f93d4be193bd41e
Albert Tian <alberttian@alberts-air.lan>
2021-01-22 23:56:55 -0500

    WATCH EXPERIMENT

--- src/templates/watch.js
+++ src/templates/watch.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PageHeader from '../components/PageHeader'
 import {liveDECAL} from '../utils/links'
 import '../css/pages/watch.scss'
+import { Player, Chat } from 'livestreams';
 
 export default function WatchPage(props) {
   return (
@@ -17,6 +18,16 @@ export default function WatchPage(props) {
           <a className="home__button blue__sp21" href={liveDECAL} target="_blank">Watch DeCal Infosession</a>
         </div>
       </div>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/yFC5y352RaM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe width="560" height="315" src="https://www.youtube.com/live_chat?v=yFC5y352RaM&embed_domain=staging.innovativedesign.club/watch" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe width="560" height="315" src="https://www.youtube.com/live_chat?v=yFC5y352RaM&embed_domain=www.staging.innovativedesign.club/watch" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <Player source="youtube" id="yFC5y352RaM" autoPlay/>
+      <Chat source="youtube" id="yFC5y352RaM"/>
     </div>
   )
 }
+
+
+// http://localhost:8000/
+// https://www.youtube.com/embed/yFC5y352RaM
+// https://www.youtube.com/live_chat?v=yFC5y352RaM&embed_domain=localhost:8000
