625a9125d9df25d216e3aef9662970d1ead6d42a
Albert Tian <atian@berkeley.edu>
2021-04-29 23:01:19 -0400

    content: officer board

--- src/templates/watch.js
+++ src/templates/watch.js
@@ -10,12 +10,12 @@ export default function WatchPage(props) {
     <div className="g__page__wrapper watch">
       <PageHeader
         pageIndicator="Watch"
-        title="Club Infosession"
-        description="Future livestreams will be posted here. Both club and decal applications have closed, check back next semester!"
+        title="Officer Infosession"
+        description="Come join our officer board!"
       />
       <div className="watch__button__wrapper">
-        <Button color="blue" href={"https://youtu.be/gXO-ENcg1Zk"}>Saturday livestream</Button>
-        <Button color="blue" href={"https://youtu.be/5jPkz14a9AE"}>Sunday livestream</Button>
+        <Button color="blue" href={"https://berkeley.zoom.us/j/91551397401"}>Officer Infosession</Button>
+        {/* <Button color="blue" href={"https://youtu.be/5jPkz14a9AE"}>Sunday livestream</Button> */}
       </div>
     </div>
   )
